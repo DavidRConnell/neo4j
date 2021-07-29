e6b5d89cf0a07d9f16ba23812cd415d38836b081
David R Connell <davidconnell12@gmail.com>
2021-07-28 23:37:23 -0500

    Init: define neo4j derivation

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      jre = pkgs.openjdk11;
+    in {
+      packages.x86_64-linux.neo4j = (pkgs.stdenv.mkDerivation rec {
+        pname = "neo4j";
+        version = "4.3.2";
+
+        src = pkgs.fetchurl {
+          url =
+            "https://neo4j.com/artifact.php?name=neo4j-community-${version}-unix.tar.gz";
+          sha256 =
+            "3474f3ec9da57fb627af71652ae6ecbd036e6ea689379f09e77e4cd8ba4b5515";
+        };
+
+        nativeBuildInputs = [ pkgs.makeWrapper pkgs.bashInteractive ];
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p "$out/share/neo4j"
+          cp -R * "$out/share/neo4j"
+
+          compgen_wrapper="$out/share/neo4j/bin/compgen"
+          cat << _EOF_ > $compgen_wrapper
+          "${pkgs.bashInteractive}/bin/bash" -c 'compgen "\$@"'
+          _EOF_
+          chmod +x $compgen_wrapper
+
+          mkdir -p "$out/bin"
+          for NEO4J_SCRIPT in neo4j neo4j-admin cypher-shell
+          do
+              makeWrapper "$out/share/neo4j/bin/$NEO4J_SCRIPT" \
+                  "$out/bin/$NEO4J_SCRIPT" \
+                  --prefix PATH : "${
+                    pkgs.lib.makeBinPath [ jre pkgs.which pkgs.gawk ]
+                  }:$out/share/neo4j/bin/" \
+                  --set JAVA_HOME "${jre}"
+          done
+
+          data_home=/home/voidee/.local/share/neo4j
+          substituteInPlace "$out/share/neo4j/conf/neo4j.conf" \
+            --replace '#dbms.directories.logs=logs' "dbms.directories.logs=$data_home/logs" \
+            --replace '#dbms.directories.run=run' "dbms.directories.run=$data_home/run" \
+            --replace '#dbms.directories.data=data' "dbms.directories.data=$data_home/data" \
+            --replace '#dbms.security.auth_enabled=false' 'dbms.security.auth_enabled=false'
+
+          runHook postInstall
+        '';
+
+        meta = with pkgs.lib; {
+          description =
+            "A highly scalable, robust (fully ACID) native graph database";
+          homepage = "http://www.neo4j.org/";
+          license = licenses.gpl3Only;
+
+          maintainers = [ maintainers.offline ];
+          platforms = pkgs.lib.platforms.unix;
+        };
+      });
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.neo4j;
+    };
+}
